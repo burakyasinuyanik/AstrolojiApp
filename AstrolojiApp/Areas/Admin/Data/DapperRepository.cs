8fc76889d4ff851f775797394eecb9b15d429da8
tunacane <tunacaneski@gmail.com>
2024-11-07 23:15:41 +0300

    homepage
    homepage

--- AstrolojiApp/Areas/Admin/Data/DapperRepository.cs
+++ AstrolojiApp/Areas/Admin/Data/DapperRepository.cs
@@ -11,8 +11,17 @@ public class DapperRepository<T> : IRepository<T> where T : class
     private readonly string _tableName;
         public DapperRepository()
         {
-             _connectionString = "Server=.\\SQLEXPRESS; Database=AstrologyDb; Integrated Security=True;TrustServerCertificate=True";
+
+        //Bağlantıyı hazırlıyoruz
+        _connectionString = "Server=localhost,1441;Database=PortfolioDb;User=sa;Password=YourStrong@Passw0rd,;TrustServerCertificate=true";
+      //  var connection = new SqlConnection(connectionString);
+
+
+      //  _connectionString = "Server=.\\SQLEXPRESS; Database=AstrologyDb; Integrated Security=True;TrustServerCertificate=True";
+            
+            
              _tableName=typeof(T).Name;
+           
              if(_tableName.EndsWith("y")){
 
                 _tableName=_tableName.Substring(0,_tableName.Length-1)+"ies";
