e4c936cbce52174644eac98519517bed91e9a3f2
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2024-11-11 21:36:36 +0300

    bug fix

--- AstrolojiApp/Controllers/HomeController.cs
+++ AstrolojiApp/Controllers/HomeController.cs
@@ -13,27 +13,39 @@ public class HomeController : Controller
 
     private readonly IRepository<AppSetting> _appsetting;
     private readonly IRepository<AstrologComment> _astrologComment;
+    private readonly IRepository<DailyNew> _dailyNew;
+    private readonly IRepository<Horoscope> _horoscope;
+    private readonly IRepository<HoroscopeGroup> _horoscopeGroup;
+    private readonly IRepository<Service> _service;
 
 
-
-    public HomeController(IRepository<AppSetting> appsetting, IRepository<AstrologComment> astrologComment)
+    public HomeController(IRepository<AppSetting> appsetting, IRepository<AstrologComment> astrologComment, IRepository<DailyNew> dailyNew, IRepository<Horoscope> horoscope, IRepository<HoroscopeGroup> horoscopeGroup, IRepository<Service> service)
     {
         _appsetting = appsetting;
         _astrologComment = astrologComment;
+        _dailyNew = dailyNew;
+        _horoscope = horoscope;
+        _horoscopeGroup = horoscopeGroup;
+        _service = service;
     }
 
     public async  Task<IActionResult> Index()
     {
           var applist= await _appsetting.GetAllAsync();
-          var comment= await _astrologComment.GetAllAsync();
-
-  //      var astrogetbyOne = await _astrologComment.GetById(5);
+          var comments= await _astrologComment.GetAllAsync();
+          var dailyNews = await _dailyNew.GetAllAsync();
+          var horoscopes = await _horoscope.GetAllAsync();
+          var horoscopeGroups=await _horoscopeGroup.GetAllAsync();
+          var services = await _service.GetAllAsync();
 
         HomePageModel homePageModel = new HomePageModel();
 
         homePageModel.AppSetting=applist.First();
-        homePageModel.AstrologComments=comment;
-            
+        homePageModel.AstrologComments=comments;
+        homePageModel.DailyNews = dailyNews;
+        homePageModel.Horoscopes=horoscopes;
+        homePageModel.Services=services;
+        homePageModel.HoroscopeGroups=horoscopeGroups;
 
         return View(homePageModel);
     }
