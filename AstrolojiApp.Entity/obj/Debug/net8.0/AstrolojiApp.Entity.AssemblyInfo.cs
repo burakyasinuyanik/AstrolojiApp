117b260d3182807e5ed6ad018b61f8f798e5b8be
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2024-12-27 23:24:21 +0300

    ok

--- AstrolojiApp.Entity/obj/Debug/net8.0/AstrolojiApp.Entity.AssemblyInfo.cs
+++ AstrolojiApp.Entity/obj/Debug/net8.0/AstrolojiApp.Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AstrolojiApp.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b836ccfbd365b56b0a74db3cf9c79329569f0752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0037ed3df690017f78b67d3ec62879cfa28fbe73")]
 [assembly: System.Reflection.AssemblyProductAttribute("AstrolojiApp.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AstrolojiApp.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AstrolojiApp.Shared/obj/Debug/net8.0/AstrolojiApp.Shared.AssemblyInfo.cs
+++ AstrolojiApp.Shared/obj/Debug/net8.0/AstrolojiApp.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AstrolojiApp.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b836ccfbd365b56b0a74db3cf9c79329569f0752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0037ed3df690017f78b67d3ec62879cfa28fbe73")]
 [assembly: System.Reflection.AssemblyProductAttribute("AstrolojiApp.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AstrolojiApp.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
