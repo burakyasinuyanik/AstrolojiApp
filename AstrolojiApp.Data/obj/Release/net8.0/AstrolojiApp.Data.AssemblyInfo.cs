6a35ce5f2a2e0a51f1e7c4ada8c485aabfed10f8
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-01-26 02:06:16 +0300

    bug fix

--- AstrolojiApp.Data/obj/Release/net8.0/AstrolojiApp.Data.AssemblyInfo.cs
+++ AstrolojiApp.Data/obj/Release/net8.0/AstrolojiApp.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AstrolojiApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13fceccfa396dbfd44e15d03d44740dc1d570763")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396ac2cd50c58a943b8a0124268b014a536445a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AstrolojiApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AstrolojiApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AstrolojiApp.Shared/Dtos/HoroscopeGroupUpdateDto.cs
+++ AstrolojiApp.Shared/Dtos/HoroscopeGroupUpdateDto.cs
@@ -5,6 +5,8 @@ namespace AstrolojiApp.Shared.Dtos;
 public class HoroscopeGroupUpdateDto
 {
     public int Id { get; set; }
+    public string Name { get; set; }
+    public string HoroscopeNames { get; set; }
     public string? Image { get; set; }
    
 }
